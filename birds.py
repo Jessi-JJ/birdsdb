cff9c32e94f7c12759fbcdd3c35f992424094a0d
Jessi Jaramillo <jaramij2@sunypoly.edu>
2025-02-25 18:46:39 -0500

    Update birds.py

--- birds.py
+++ birds.py
@@ -6,12 +6,16 @@ df = pd.read_csv('bothcountries_data_format_19feb2019_abundance_format.csv', enc
 
 # KPIs
 st.title("Canopy tree preference by insectivorous birds on shade-coffee farms")
-st.metric("Total Birds Observed", f"{df['num_birds'].sum():,.2f}")
-st.metric("Total Orders", len(df))
+st.metric("Total Birds Observed", f"{df['num_birds'].sum()}")
+st.metric("Total Observations", len(df))
 
 df_treesp = df["treesp"].value_counts().to_frame()
 df_treesp.reset_index(inplace=True)
 df_treesp.columns = ["treesp", "count"]
+
+df_birdsp = df["bird_sp"].value_counts().to_frame()
+df_birdsp.reset_index(inplace=True)
+df_birdsp.columns = ["bird_sp", "count"]
 
 col1, col2 = st.columns((2))
 
@@ -21,14 +25,20 @@ with col1: # Chart
                  template = "seaborn")
   st.plotly_chart(fig,use_container_width=True, height = 200)
 
-st.sidebar.header("Choose your filter: ")
-# Create for Country
-country = st.sidebar.multiselect("Pick a Country", df["country"].unique())
-if not country:
-    df2 = df.copy()
-else:
-    df2 = df[df["country"].isin(country)]
+with col2: #Chart
+  st.subheader("Bird Species")
+  fig = px.bar(df_birdsp, x = "bird_sp", y = "count", text = [f"{x}" for x in df_birdsp["bird_sp"]],
+                 template = "seaborn")
+  st.plotly_chart(fig,use_container_width=True, height = 200)
 
-# Data Table
-st.subheader("Birds Data")
-st.dataframe(df)
+# Create a multiselect for Country in the sidebar
+country = st.sidebar.multiselect("Select Country", df["Country"].unique())
+
+# Filter the DataFrame based on the Country selection
+if country:
+    filtered_df = df[df["Country"].isin(country)]
+else:
+    filtered_df = df.copy()
+
+# Display the filtered DataFrame
+st.write("Filtered DataFrame", filtered_df)
