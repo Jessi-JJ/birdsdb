8d08d7e41c0d02fc48a868f593e8b8c869c3527e
Jessi Jaramillo <jaramij2@sunypoly.edu>
2025-02-25 18:14:01 -0500

    Create birds.py

---
+++ birds.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('bothcountries_data_format_19feb2019_abundance_format.csv', encoding='latin-1')
+
+# KPIs
+st.title("Canopy tree preference by insectivorous birds on shade-coffee farms")
+st.metric("Total Birds Observed", f"${df['num_birds'].sum():,.2f}")
+st.metric("Total Orders", len(df))
+
+col1, col2 = st.columns((2))
+
+with col1: # Chart
+  st.subheader("Tree Species")
+  fig = px.bar(category_df, x = "treesp", y = "count", text = ['${:,.2f}'.format(x) for x in category_df["treesp"]],
+                 template = "seaborn")
+  st.plotly_chart(fig,use_container_width=True, height = 200)
+
+st.sidebar.header("Choose your filter: ")
+# Create for Country
+country = st.sidebar.multiselect("Pick a Country", df["country"].unique())
+if not country:
+    df2 = df.copy()
+else:
+    df2 = df[df["country"].isin(country)]
+
+# Data Table
+st.subheader("Birds Data")
+st.dataframe(df)
